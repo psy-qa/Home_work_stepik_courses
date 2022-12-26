f53078ae5a6abc31014f9e3717274f2612d188b4
RaveHazard <ravehazard1@gmail.com>
2022-12-27 01:30:11 +0500

    add imports

--- Slenium_stepik/conftest.py
+++ Slenium_stepik/conftest.py
@@ -1,5 +1,7 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 link = "http://selenium1py.pythonanywhere.com/"
 
