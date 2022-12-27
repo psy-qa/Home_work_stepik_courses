4b7cd5ee007c221d3fc78c8cf5a47003b9976705
RaveHazard <ravehazard1@gmail.com>
2022-12-27 20:19:59 +0300

    fix test

--- Slenium_stepik/chenge_lang_test/test_items.py
+++ Slenium_stepik/chenge_lang_test/test_items.py
@@ -1,9 +1,7 @@
-import pytest
-from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 def test_button_value_in_page(browser):
-    """Open browser and go to link"""
+    """Check button ang attributes button in page"""
     button = browser.find_element(By.XPATH, '//*[@id="add_to_basket_form"]/button')
     button_text = button.get_attribute("value")
 
