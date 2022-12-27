ab673ef6127be80acf5cc899548b756a43d121ea
RaveHazard <ravehazard1@gmail.com>
2022-12-27 20:45:09 +0300

    edit conftest.py

--- Slenium_stepik/chenge_lang_test/conftest.py
+++ Slenium_stepik/chenge_lang_test/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default=None,
@@ -15,17 +16,24 @@ def invite_print():
 def browser(request):
     language = request.config.getoption("language")
     if language == "fr":
-        print("You choose fr language, start test....")
-        browser = webdriver.Chrome()
-        browser.get(f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/")
+        print('\n You choose fr language, start test....')
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+        browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
     elif language == "ru":
         print("You choose ru language, start test....")
-        browser = webdriver.Chrome()
-        browser.get(f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/")
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+        browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
     elif language == "es":
         print("You choose es language, start test....")
-        browser = webdriver.Chrome()
-        browser.get(f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/")
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+        browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
     else:
         raise pytest.UsageError("=======you need choose lang ru, es or fr========")
 
